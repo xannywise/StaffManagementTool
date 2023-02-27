6d72c631a0ca5293a7c35b0a53b94cfec7619b03
Q506869 <maksim.hermann@bmw.de>
2023-02-27 11:28:29 +0100

    Maksim - setting up navigation through app

--- 01_repo/StaffManagementTool/StaffManagementTool/settings.py
+++ 01_repo/StaffManagementTool/StaffManagementTool/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'StaffManagementTool.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(SETTINGS_PATH, './templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
